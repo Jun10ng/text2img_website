cf4a166a9da87c45747a9a349623cc2d1fb66d07
Jun10ng <fu4ng@foxmail.com>
2019-08-14 12:29:33 +0800

    add file operate of translate script

--- text2img_website/view.py
+++ text2img_website/view.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from tool.translate import translate
 
 def text(request):
     if request.method == 'GET':
         return render(request,'text.html')
     elif request.method=='POST':
         text = request.POST['text']
+        text_en = translate(text)
+        # 单用户操作
+        with open('text/text_en.txt','w') as f:
+            f.write(text_en)
         return render(request,"showimg.html",{"text":text})
 
 def img(request):
